0f743fe8a2dae6ed9280fe4969ec81b2669f6b9e
robin* <robin_maki@planet.moe>
2018-08-18 18:35:16 +0900

    check 함수와 set 함수로 나눔

--- check-limit.js
+++ check-limit.js
@@ -6,19 +6,32 @@ const dayLimit = ms(process.env.DAY_LIMIT || '1d');
 
 fs.existsSync(path.resolve(__dirname, 'data')) || fs.mkdirSync(path.resolve(__dirname, 'data'));
 
-module.exports = (acct) => {
-    return new Promise((success) => {
-        fs.readdir(path.resolve(__dirname, 'data', acct), (err, content) => {
-            if(err) {
-                return success(true);
-            }
-            if(Date.now() - Number(content) >= dayLimit) {
-                return success(true);
-            }
-            else {
-                return success(false);
-            }
+module.exports = {
+    check(acct) {
+        return new Promise((success) => {
+            fs.readFile(path.resolve(__dirname, 'data', acct), (err, content) => {
+                if(err) {
+                    return success(true);
+                }
+                if(Date.now() - Number(content) >= dayLimit) {
+                    return success(true);
+                }
+                else {
+                    return success(false);
+                }
+            });
         });
-    })
-    
+    },
+    set(acct) {
+        return new Promise((success, fail) => {
+            fs.writeFile(path.resolve(__dirname, 'data', acct), Date.now().toString(), (err) => {
+                if(!err) {
+                    success();
+                }
+                else {
+                    fail(err);
+                }
+            });
+        });
+    }
 };
