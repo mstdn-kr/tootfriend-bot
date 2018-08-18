1ba1502106bbb1ffa8058bf1aae8df8e9fdc71f2
robin* <robin_maki@planet.moe>
2018-08-18 21:48:59 +0900

    체크 함수에 계정 ID도 추가

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const m = new Mastodon({
     access_token: process.env.ACCESS_TOKEN,
     api_url: process.env.API_URL
 });
+limit.init(m);
 
 const notice = m.stream('streaming/user');
 
@@ -22,7 +23,7 @@ const tag = m.stream('streaming/hashtag', {tag: '툿친소'});
 tag.on('message', async(msg) => {
     console.log(msg);
     if(msg.event === 'update' && !msg.data.in_reply_to_id && !msg.data.reblog) {
-        if(await limit.check(msg.data.account.acct)) {
+        if(await limit.check(msg.data.account.acct, msg.data.account.id)) {
             await m.post(`statuses/${msg.data.id}/reblog`, {});
             limit.set(msg.data.account.acct);
         }
