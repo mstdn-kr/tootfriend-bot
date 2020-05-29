a2a6d338a94368f0aa2d2e010cc19c2954a03256
robin* <robin_maki@planet.moe>
2020-05-29 19:44:58 +0900

    디버그용 콘솔 로그 머릿말 업데이트

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ limit.init(m);
 const notice = m.stream('streaming/user');
 
 notice.on('message', (msg) => {
+    console.log('[notice]', msg);
     if(msg.event === 'notification' && msg.data.type === 'follow') {
         m.post('follows', {uri: msg.data.account.acct});
     }
@@ -21,7 +22,7 @@ notice.on('message', (msg) => {
 const tag = m.stream('streaming/hashtag', {tag: '툿친소'});
 
 tag.on('message', async(msg) => {
-    console.log(msg);
+    console.log('[update]', msg);
     if(msg.event === 'update' && !msg.data.in_reply_to_id && !msg.data.reblog) {
         if(await limit.check(msg.data.account.acct, msg.data.account.id)) {
             await m.post(`statuses/${msg.data.id}/reblog`, {});
